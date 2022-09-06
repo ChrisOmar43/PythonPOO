94eef8efadd43fec41a7fbf21e87bee776e3246c
ChrisOmar43 <chrisomar08@hotmail.com>
2022-09-06 16:06:45 -0500

    Añado abstraccion y pruebas con decoradores

---
+++ pruebas_decoradores.py
@@ -0,0 +1,14 @@
+def funcion_decoradora(funcion):
+    print('.')
+	def wrapper():
+		print("Este es el último mensaje...")
+		funcion()
+		print("Este es el primer mensaje ;)")
+	return wrapper
+
+def zumbido():
+	print("Buzzzzzz")
+
+zumbido = funcion_decoradora(zumbido)
+
+zumbido()
